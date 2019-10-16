c7fa8b4ec31670bad520076afb1809374f5f298e
Jeffrey Huddleston <jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.com>
2019-10-16 17:18:15 -0400

    image now appearing in modle

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var friendList = [
     {
       name: 'Jeffrey Huddleston',
-      profilePic: 'https://avatars2.githubusercontent.com/u/52114016?s=400&u=037a74a4df1f95db11804ae881a6e9cdfe4bd3f3&v=4',
+      picture: 'https://avatars2.githubusercontent.com/u/52114016?s=400&u=037a74a4df1f95db11804ae881a6e9cdfe4bd3f3&v=4',
       scores: [
         "4",
         "4",
